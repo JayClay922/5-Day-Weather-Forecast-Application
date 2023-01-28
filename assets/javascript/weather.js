1208852a4114faf5285bcd47b8a5297c8e24faeb
JayClay922 <jamesclayman922@live.com>
2023-01-28 18:41:26 +0000

    adding api key, query url and ajax method to event listener function

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,13 +1,15 @@
-$("#search-input")
-let weatherAPI = "0116efa5772e70096506b1a5c5e2d55b"
-let queryURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0116efa5772e70096506b1a5c5e2d55b}"
+$("#search-input").on("click", function (event) {
+    let weatherAPI = "0116efa5772e70096506b1a5c5e2d55b"
+    let queryURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0116efa5772e70096506b1a5c5e2d55b}"
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}) .then(function(response) {
-  console.log(queryURL)
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(queryURL)
+    });
+})
+
 
 
 
