58930b596a036ad9f87bb420dce08e30325c27a8
JayClay922 <jamesclayman922@live.com>
2023-01-28 20:12:08 +0000

    updating code to find the longitude and latitude of location and that calling that through another AJAX method to find the weather forecast of that location

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,13 +1,45 @@
-$("#search-input").on("click", function (event) {
-    let weatherAPI = "0116efa5772e70096506b1a5c5e2d55b"
-    let queryURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0116efa5772e70096506b1a5c5e2d55b}"
+// $("#search-input").on("click", function (event) {
+//     let weatherAPI = "0116efa5772e70096506b1a5c5e2d55b"
+//     let queryURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=0116efa5772e70096506b1a5c5e2d55b"
 
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         console.log(queryURL)
+//     });
+// });
+
+$("#search-button").on("click", function() {
+    let city = $("#search-input").val();
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(queryURL)
-    });
+        url: "http://api.openweathermap.org/geo/1.0/direct",
+        method: "GET",
+        data: {
+           key: "0116efa5772e70096506b1a5c5e2d55b",
+           q: city
+        },
+        success: function(location) {
+            let lat = location.results[0].geometry.lat;
+            let lng = location.results[0].geometry.lat;
+
+            $.ajax({
+                url: "api.openweathermap.org/data/2.5/forecast",
+                method: "GET",
+                data: {
+                    lat: lat,
+                    lon: lng,
+                    appid: "0116efa5772e70096506b1a5c5e2d55b"
+                },
+                success: function(weather) {
+                    
+                }
+            })
+        }
+        
+        })
+
+    })
 })
 
 
