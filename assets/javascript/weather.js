a7998182d8313a01360eaf99504c62e88cd5a118
JayClay922 <jamesclayman922@live.com>
2023-01-29 10:49:10 +0000

    adding comments to code

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,8 +1,13 @@
 let apiKey = "0116efa5772e70096506b1a5c5e2d55b"
 
+//Adding event listener for when the button with the ID of 'search-button' is clicked.
 $("#search-button").on("click", function (event) {
     event.preventDefault();
+    
+    //Getting the value of what the user inputs by targetting the ID of the search input area.
     let city = $("#search-input").val();
+
+    //Using AJAX to make an API call requesting longitude and latitude of the location inputted by the user.
     $.ajax({
         url: "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey,
         method: "GET",
@@ -11,7 +16,9 @@ $("#search-button").on("click", function (event) {
         let lat = location[0].lat
         let lng = location[0].lon
         console.log(location)
+        v
 
+        //Another API call requesting weather information of the location with the longitude and latitude retrieved by first API call.
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/forecast?" + "lat=" + lat + "&lon=" + lng + "&appid=" + apiKey,
             lat: lat,
